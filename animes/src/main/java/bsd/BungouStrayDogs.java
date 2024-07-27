7d17d18cb3a124f1c288fe5c5f215c95fb29e19f
Beatriz Narciso <147112758+beatriznarf@users.noreply.github.com>
2024-07-27 20:31:20 -0300

    Configurando banco e aplicação
    Nessa parte do projeto, fiz a configuração do banco de dados no "resources", além de ter criado a primeira aplicação do projeto.

---
+++ animes/src/main/java/bsd/BungouStrayDogs.java
@@ -0,0 +1,19 @@
+package bsd;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class BungouStrayDogs {
+    public static void main(String[] args) {
+
+        SpringApplication.run(BungouStrayDogs.class, args);
+    }
+        @Bean
+        public ModelMapper modelMapper () {
+            return new ModelMapper();
+        }
+
+}
